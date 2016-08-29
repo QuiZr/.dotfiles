de6b6f15f6500cf1f9bf92156d581e9925373fa5
Tymoteusz Adamczyk <tymoteusz.adamczyk@outlook.com>
2016-08-29 18:18:41 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -23,3 +23,5 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/$file
 done
+
+echo "Done! In order for the changes to take effect, please restart your terminal."
