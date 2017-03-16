cc181d749922fe9996b94758f574314500dde83d
Tymoteusz Adamczyk <tymoteusz.adamczyk@outlook.com>
2017-03-16 20:29:58 +0100

    Force en_US language once again

--- step1.sh
+++ step1.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
-    sudo update-locale LANG=en_US.UTF8
+#force en_US.UTF-8 language
+sudo update-locale LANG=en_US.UTF8
+sudo update-locale LC_ALL=en_US.UTF8
+sudo locale-gen --purge en_US.UTF8
 
-    sudo add-apt-repository ppa:git-core/ppa
-    sudo apt update
-    sudo apt install git wget zsh
-    sudo apt upgrade
-    sudo apt-get autoremove --purge
+#install packages
+sudo add-apt-repository ppa:git-core/ppa
+sudo apt update
+sudo apt install git wget zsh
+sudo apt upgrade
+sudo apt-get autoremove --purge
 
 git clone --recursive https://github.com/zsh-users/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
